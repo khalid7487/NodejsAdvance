b9dcc5b95e46f4618f4993097d060c3adf04d77a
khalid7487 <khalid15-7487@diu.edu.bd>
2022-06-06 23:57:30 +0600

    define keys

--- services/cache.js
+++ services/cache.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
+//this is the original copy of exac form monogodb
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
     console.log('IM ABOUT TO RUN A QUERY');
+    
+    const key =Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name
+    });
 
-    return exec.apply(this, arguments)
+    console.log(key);
+
+    return exec.apply(this, arguments);
 }
+
