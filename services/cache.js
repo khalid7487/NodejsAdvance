f808f9e94b86f4c1dc170ae8a0674475257a2796
khalid7487 <khalid15-7487@diu.edu.bd>
2022-06-08 00:18:17 +0600

    redis hydrating model

--- services/cache.js
+++ services/cache.js
@@ -1,17 +1,39 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 
 //this is the original copy of exac form monogodb
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
-    console.log('IM ABOUT TO RUN A QUERY');
+mongoose.Query.prototype.exec = async function () {
+
+    const key = JSON.stringify(
+        Object.assign({}, this.getQuery(), {
+            collection: this.mongooseCollection.name
+        })
+    );
+
+    //see if we have a value for 'key' in redis
+    const cacheValue = await client.get(key);
+
+    //if we do, return that 
+    if(cacheValue){
+        // console.log(cacheValue);
+        const doc =new this.model(JSON.parse(cacheValue));
+
+        return doc;
+    }
+
+    //otherwise, issue the query and store the result in redis
+    const result = exec.apply(this, arguments);
+
+    console.log(result);
+    client.set(key, JSON.stringify(result));
     
-    const key =Object.assign({}, this.getQuery(), {
-        collection: this.mongooseCollection.name
-    });
-
-    console.log(key);
-
-    return exec.apply(this, arguments);
+    return result;
 }
 
